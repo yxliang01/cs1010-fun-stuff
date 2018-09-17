c3c4039da4ced06523da4f72247ca2ff71159d8f
yxliang01 <13267.okk@gmail.com>
2018-09-17 20:06:29 +0800

    Fixed bug in tutorial 04 problem 11.2.c skeleton code

--- tutorial/04/problem-11.2.c-skeleton.c
+++ tutorial/04/problem-11.2.c-skeleton.c
@@ -8,9 +8,9 @@ int main(void) {
 
   const long answer = 1000000; // TODO: CHANGE ME
 
-  const char[] STR_TOO_HIGH = "too high";
-  const char[] STR_TOO_LOW = "too low";
-  const char[] STR_CORRECT = "CORRECT!";
+  const char STR_TOO_HIGH[] = "too high";
+  const char STR_TOO_LOW[] = "too low";
+  const char STR_CORRECT[] = "CORRECT!";
 
   // Read guess and feedback to user
   const long guess = cs1010_read_long();
